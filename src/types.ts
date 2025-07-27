091adac2a36616b416c1cbf756345e626feb3494
Alexandre Moreira <alexandre.rocha@estudante.ufjf.br>
2025-07-27 15:24:04 -0300

    add definnição inicial de tipos

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Naipe = '♠' | '♥' | '♦' | '♣';
+
+export type ValorCarta = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+
+export interface Card {
+  naipe: Naipe;
+  valor: ValorCarta;
+}
