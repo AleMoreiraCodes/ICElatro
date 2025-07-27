39849052f5733ff84e0cacc0ff737d894c65e302
Alexandre Moreira <alexandre.rocha@estudante.ufjf.br>
2025-07-27 17:21:33 -0300

    add estrutura inicial de montagem e distribuição dos decks

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,40 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css' 
+import { Deck } from './core/deck';
+import type { Carta } from './types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    teste
   </div>
 `
+ 
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+ 
+function main(): void {
+  const deck = new Deck();
+
+  const cartas: Carta[] = deck.getCartas();
+
+  console.log('🃏 Cartas iniciais (aleatórias):');
+  cartas.forEach((carta, i) => {
+    console.log(`Carta ${i + 1}: ${carta.valor} ${carta.naipe}`);
+  });
+
+
+  deck.embaralhar();
+
+  console.log('\n🃏 Cartas embaralhadas:')
+  cartas.forEach((carta, i) => {
+    console.log(`Carta ${i + 1}: ${carta.valor} ${carta.naipe}`);
+  });
+
+  console.log('\n🃏 Distribuindo 8 cartas:', deck.distribuir());
+
+  console.log('🃏 Cartas finais (aleatórias):');
+  cartas.forEach((carta, i) => {
+    console.log(`Carta ${i + 1}: ${carta.valor} ${carta.naipe}`);
+  });
+
+}
+
+main()
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,8 @@ export type Naipe = '♠' | '♥' | '♦' | '♣';
 
 export type ValorCarta = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
 
-export interface Card {
+export interface Carta {
   naipe: Naipe;
-  valor: ValorCarta;
+  valor: ValorCarta; 
 }
+
